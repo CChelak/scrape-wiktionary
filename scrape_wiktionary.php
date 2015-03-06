553ef3dd5dc173ec9834b40d2f9dbee911242e5d
CChelak <CChelak@users.noreply.github.com>
2015-03-06 14:55:19 -0700

    Update scrape_wiktionary.php
    Latest edition makes program save to a file... I think. Somebody will have to check it, because my compiler would not create the file. It may be something wrong with my syntax. I'm not sure.

--- scrape_wiktionary.php
+++ scrape_wiktionary.php
@@ -6,26 +6,18 @@
 	$regex = "~<li>(.+?)</li>~";
 	// regular expression for "get the URL link for the next page" (kinda)
 	$nextLinkRegex = "~/w/index.php?(.+?)#mw-pages~";
-	$linkfound = true;
+	$linkfound = true;	//used to test if a correct link was found; if not, then the search loop will end
+
+	$fname = "C:\\listOfUncountableNouns.txt";	//Name of file to save the words
+	$fp = fopen($fname, "w");
 	
 	// do this 3 times (for now while debugging)
-	for ($i=0; $i<10000 && $linkfound == true; $i++) {
+	for ($i=0; $i<10 && $linkfound == true; $i++) {
 		//if($i >= 1) {echo "say anything <br>";}
 		// read the entire webpage and store as a string
 		$data = file_get_contents($nextURL);
 		//if($i >= 1){echo "$data<br><br>";}
-		
-		/* //A small sample of what is inside $data. It checks to see where the '&' symbols were for a previous test
-		for ($k = 0; $k<1000; $k++){
-			if ($data[$k] === '&'){
-				echo "*******$data[$i]******";
-			}
-			else{
-				echo " $data[$k] ";
-			}
-		}
-		echo "<br><br>";*/
-		
+				
 		for ($j = 0; $j<2; $j++){
 		  $foundamp = strpos($data, "amp;");
 			if ($foundamp !== false){
@@ -37,15 +29,25 @@
 		preg_match_all($regex,$data,$matches);
 		
 		// print each <li> entry to the page
+		$wordlist = "";	//holds the large list of words found on the page
+		
 		foreach ($matches[1] as $match) {
-			echo "$match <br>";
+			$wordBegin = strpos ($match, "title=") + 7;
+			$wordEnd = strpos ($match, "\">", $wordBegin);
+			$foundWord = substr ($match, $wordBegin, $wordEnd - $wordBegin);
+			if ($foundWord != "Category:English singularia tantum" && $foundWord != "Category:Uncountable nouns by language"){
+				$wordlist = $wordlist . $foundWord . "<br>";
+			}
 		}
+		echo "$wordlist";
+		fwrite ($fp, $wordlist);
+		
 		
 		// find the next page url
 		$oldlinkpos = 0;	//holds beginning position of previously found matching link
 		$newlinkpos = 0;	//holds beginning position of newly found matching link
 		$linklength = 300;	//set at 300 so the loop can begin. In "for" loop, condition set in case wrong url was found
-					//or in other words, if something was found that was too long to be a url, program would keep searching
+						  	//or in other words, if something was found that was too long to be a url, program would keep searching
 		$linkending = 0;
 		
 		for($m = 0; $m <=8 && $linklength >= 300 && $newlinkpos !== false; $m++){
@@ -73,4 +75,5 @@
 			}
 		}
 	}
+		fclose($fp);
 ?>
